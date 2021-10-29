34fd1730fefd77417f33effbd7b0f8a387a3a88b
Liron <lironpaz7@gmail.com>
2021-10-29 21:31:03 +0300

    Version: 1.1.5 Calendar upgrade - CustomCalendar

--- app/src/main/java/com/gdsc_technion/gdsc_app/CalendarFragment.kt
+++ app/src/main/java/com/gdsc_technion/gdsc_app/CalendarFragment.kt
@@ -5,6 +5,7 @@ import android.net.Uri
 import android.os.Bundle
 import android.os.Parcelable
 import android.provider.CalendarContract
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,10 +17,14 @@ import com.gdsc_technion.gdsc_app.databinding.FragmentCalenderBinding
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.parcelize.Parcelize
+import org.naishadhparmar.zcustomcalendar.CustomCalendar
+import org.naishadhparmar.zcustomcalendar.OnNavigationButtonClickedListener
+import org.naishadhparmar.zcustomcalendar.Property
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
 import kotlin.collections.HashMap
+import kotlin.collections.set
 
 @Parcelize
 data class EventData(
@@ -38,10 +43,17 @@ data class EventData(
  * Use the [CalendarFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class CalendarFragment : Fragment() {
+class CalendarFragment : Fragment(), OnNavigationButtonClickedListener {
     private lateinit var binding: FragmentCalenderBinding
     private var currentDateSelected: String? = null
-    private lateinit var cal: Calendar
+
+    // calendar
+    private lateinit var calendar: Calendar
+    private lateinit var dateParser: SimpleDateFormat
+    private lateinit var descHashMap: HashMap<Any, Property>
+    private lateinit var dateHashMap: HashMap<Int, Any>
+    private lateinit var customCalendar: CustomCalendar
+    private lateinit var upcomingEventsTable: ArrayList<EventData>
     private lateinit var currentDateContent: TextView
     private lateinit var calenderUpcomingEvents: TextView
     private lateinit var addToCalendarBtn: ImageView
@@ -58,14 +70,19 @@ class CalendarFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         // initialize buttons, textviews and events list
-        val dateParser = SimpleDateFormat("dd/MM/yyyy")
-        cal = Calendar.getInstance()
-        val calender = binding.calendar
+        dateParser = SimpleDateFormat("dd/MM/yyyy")
+        customCalendar = binding.calendar
         val backBtn = binding.calendarBackBtn
         val todayBtn = binding.calendarTodayBtn
         val selectBtn = binding.calendarSelectBtn
         val adminButton = binding.calendarEventAdminBtn
         addToCalendarBtn = binding.calendarCurrentAddToCalendarBtn
+        descHashMap = HashMap()
+        dateHashMap = HashMap()
+
+        initializeCalendar()
+
+
 
         fAuth = FirebaseAuth.getInstance()
         val userName = fAuth.currentUser?.email.toString()
@@ -83,7 +100,7 @@ class CalendarFragment : Fragment() {
             EventAdminFragment().show(requireActivity().supportFragmentManager, null)
         }
 
-        val upcomingEventsTable = ArrayList<EventData>()
+        upcomingEventsTable = ArrayList()
 
         val currentDateTitle = view.findViewById<TextView>(R.id.calendar_current_title)
         currentDateContent = view.findViewById(R.id.calendar_current_content)
@@ -91,9 +108,9 @@ class CalendarFragment : Fragment() {
 
 
         // updates current date view title with current date
-        val day = cal.get(Calendar.DAY_OF_MONTH)
-        val month = cal.get(Calendar.MONTH)
-        val year = cal.get(Calendar.YEAR)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+        val month = calendar.get(Calendar.MONTH)
+        val year = calendar.get(Calendar.YEAR)
         val todayPresentationFormat = "$day/${month + 1}/$year"
         currentDateTitle.text = todayPresentationFormat
         val dateToday = dateParser.parse(todayPresentationFormat)
@@ -105,7 +122,7 @@ class CalendarFragment : Fragment() {
                 val docNameSplit = doc.id.split(" ")
                 val dateFormat = docNameSplit[0].replace(".", "/")
                 val sdf = SimpleDateFormat("dd.MM.yyyy HH:mm")
-                data.timeObj = sdf.parse(doc.id.toString())
+                data.timeObj = sdf.parse(doc.id)
 
                 val currentDate = dateParser.parse(dateFormat)
                 allEventsDateTable[dateFormat] = data.title.toString()
@@ -137,17 +154,25 @@ class CalendarFragment : Fragment() {
 
         }
 
-        // when choosing a date on calendar
-        calender.setOnDateChangeListener { _, year, month, day ->
-            val presentationFormat = "$day/${month + 1}/$year"
-            currentDateSelected = presentationFormat
-            currentDateTitle.text = presentationFormat
-            checkAndUpdateEvent(presentationFormat)
+        // CustomCalendar setup
+        customCalendar.setOnNavigationButtonClickedListener(CustomCalendar.PREVIOUS, this);
+        customCalendar.setOnNavigationButtonClickedListener(CustomCalendar.NEXT, this);
+        customCalendar.setOnDateSelectedListener { view, selectedDate, desc ->
+            val day = selectedDate.get(Calendar.DAY_OF_MONTH)
+            val month = selectedDate.get(Calendar.MONTH) + 1
+            val year = selectedDate.get(Calendar.YEAR)
+
+            val sDate = "$day/$month/$year"
+            currentDateSelected = sDate
+            currentDateTitle.text = sDate
+            checkAndUpdateEvent(sDate)
+
+//            Toast.makeText(context, sDate, Toast.LENGTH_SHORT).show()
         }
 
         // today button
         todayBtn.setOnClickListener {
-            calender.setDate(Calendar.getInstance().timeInMillis, true, false)
+            customCalendar.setDate(calendar, dateHashMap)
             currentDateSelected = todayPresentationFormat
             currentDateTitle.text = todayPresentationFormat
             checkAndUpdateEvent(todayPresentationFormat)
@@ -191,6 +216,37 @@ class CalendarFragment : Fragment() {
         }
     }
 
+    private fun initializeCalendar() {
+
+        val defaultProperty = Property()
+        defaultProperty.layoutResource = R.layout.default_view
+        defaultProperty.dateTextViewResource = R.id.calendar_textview
+        descHashMap["default"] = defaultProperty
+
+        val currentProperty = Property()
+        currentProperty.layoutResource = R.layout.current_view
+        currentProperty.dateTextViewResource = R.id.calendar_textview
+        descHashMap["current"] = currentProperty
+
+        val eventProperty = Property()
+        eventProperty.layoutResource = R.layout.event_view
+        eventProperty.dateTextViewResource = R.id.calendar_textview
+        descHashMap["event"] = eventProperty
+
+//        val blankProperty = Property()
+//        blankProperty.layoutResource = R.layout.blank_view
+//        blankProperty.dateTextViewResource = R.id.calendar_textview
+//        descHashMap["blank"] = blankProperty
+
+        customCalendar.setMapDescToProp(descHashMap)
+        calendar = Calendar.getInstance()
+        dateHashMap[calendar.get(Calendar.DAY_OF_MONTH)] = "current"
+
+        // set date
+        customCalendar.setDate(calendar, dateHashMap)
+
+    }
+
     private fun checkAndUpdateEvent(date: String) {
         if (date in allEventsDateTable) {
             currentDateContent.text = allEventsDateTable[date]
@@ -199,5 +255,74 @@ class CalendarFragment : Fragment() {
             currentDateContent.text = "No events"
             addToCalendarBtn.visibility = View.INVISIBLE
         }
+    }
+
+    override fun onNavigationButtonClicked(
+        whichButton: Int,
+        newMonth: Calendar?
+    ): Array<MutableMap<Int, Any>?> {
+        val arr: Array<MutableMap<Int, Any>?> = arrayOfNulls(2)
+        when (newMonth!![Calendar.MONTH]) {
+            Calendar.JANUARY -> {
+                arr[0] = updateCalenderMonth(Calendar.JANUARY)
+            }
+            Calendar.FEBRUARY -> {
+                arr[0] = updateCalenderMonth(Calendar.FEBRUARY)
+            }
+            Calendar.MARCH -> {
+                arr[0] = updateCalenderMonth(Calendar.MARCH)
+            }
+            Calendar.APRIL -> {
+                arr[0] = updateCalenderMonth(Calendar.APRIL)
+            }
+            Calendar.MAY -> {
+                arr[0] = updateCalenderMonth(Calendar.MAY)
+            }
+            Calendar.JUNE -> {
+                arr[0] = updateCalenderMonth(Calendar.JUNE)
+            }
+            Calendar.JULY -> {
+                arr[0] = updateCalenderMonth(Calendar.JULY)
+            }
+            Calendar.AUGUST -> {
+                arr[0] = updateCalenderMonth(Calendar.AUGUST)
+            }
+            Calendar.SEPTEMBER -> {
+                arr[0] = updateCalenderMonth(Calendar.SEPTEMBER)
+            }
+            Calendar.OCTOBER -> {
+                arr[0] = updateCalenderMonth(Calendar.OCTOBER)
+            }
+            Calendar.NOVEMBER -> {
+                arr[0] = updateCalenderMonth(Calendar.NOVEMBER)
+
+            }
+            Calendar.DECEMBER -> {
+                arr[0] = updateCalenderMonth(Calendar.DECEMBER)
+            }
+        }
+        return arr
+    }
+
+    private fun updateCalenderMonth(month: Int): HashMap<Int, Any> {
+        val map = HashMap<Int, Any>()
+        for (event in upcomingEventsTable) {
+            val dateSplitted = event.date?.split("/")
+            if (dateSplitted != null) {
+                val dDay = dateSplitted[0].toInt()
+                val dMonth = dateSplitted[1].toInt()
+                val dYear = dateSplitted[2].toInt()
+
+                val timeInMillis = customCalendar.selectedDate.timeInMillis
+                val currentYear = dateParser.format(timeInMillis).split("/")[2].toInt()
+
+                Log.d("year: ", dYear.toString())
+
+                if (month + 1 == dMonth && currentYear == dYear) {
+                    map[dDay] = "event"
+                }
+            }
+        }
+        return map
     }
 }
